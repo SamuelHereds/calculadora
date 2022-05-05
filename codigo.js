5fc93465639cae591a782df3cd1f6a9843377b54
Samuel Muniz <d2021005503@unifei.edu.br>
2022-05-04 23:23:00 -0300

    final

--- codigo.js
+++ codigo.js
@@ -11,7 +11,8 @@ function subtrai() {
     n2 = document.getElementById("num2").value;
     resp = parseFloat(n1) - parseFloat(n2);
     document.getElementById("result").innerText = "Resultado: " + resp;
-}
+    
+} 
 function multiplica(){
     let n1, n2, resp;
     n1 = document.getElementById("num1").value;
