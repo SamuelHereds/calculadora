c58cfb9ce578e71c15858cee3b190495bff941bc
Samuel Muniz <d2021005503@unifei.edu.br>
2022-05-13 16:39:12 -0300

    versão melhorada

--- codigo.js
+++ codigo.js
@@ -1,32 +1,4 @@
-function soma() {
-    let n1, n2, resp;
-    n1 = document.getElementById("num1").value;
-    n2 = document.getElementById("num2").value;
-    resp = parseFloat(n1) + parseFloat(n2);
-    document.getElementById("result").innerText = "Resultado: " + resp;
-}
-function subtrai() {
-    let n1, n2, resp;
-    n1 = document.getElementById("num1").value;
-    n2 = document.getElementById("num2").value;
-    resp = parseFloat(n1) - parseFloat(n2);
-    document.getElementById("result").innerText = "Resultado: " + resp;
 
-}
-function multiplica() {
-    let n1, n2, resp;
-    n1 = document.getElementById("num1").value;
-    n2 = document.getElementById("num2").value;
-    resp = parseFloat(n1) * parseFloat(n2);
-    document.getElementById("result").innerText = "Resultado: " + resp;
-}
-function divide() {
-    let n1, n2, resp;
-    n1 = document.getElementById("num1").value;
-    n2 = document.getElementById("num2").value;
-    resp = parseFloat(n1) / parseFloat(n2);
-    document.getElementById("result").innerText = "Resultado: " + resp;
-}
 function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
