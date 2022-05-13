d52e3002908c806d8b07fe7301ae9589f835b28e
Samuel Muniz <d2021005503@unifei.edu.br>
2022-05-13 16:32:25 -0300

    Versão melhorada

--- codigo.js
+++ codigo.js
@@ -11,19 +11,39 @@ function subtrai() {
     n2 = document.getElementById("num2").value;
     resp = parseFloat(n1) - parseFloat(n2);
     document.getElementById("result").innerText = "Resultado: " + resp;
-    
-} 
-function multiplica(){
+
+}
+function multiplica() {
     let n1, n2, resp;
     n1 = document.getElementById("num1").value;
     n2 = document.getElementById("num2").value;
     resp = parseFloat(n1) * parseFloat(n2);
     document.getElementById("result").innerText = "Resultado: " + resp;
 }
-function divide(){
+function divide() {
     let n1, n2, resp;
     n1 = document.getElementById("num1").value;
     n2 = document.getElementById("num2").value;
     resp = parseFloat(n1) / parseFloat(n2);
-    document.getElementById("result").innerText = "Resultado: " + resp; 
+    document.getElementById("result").innerText = "Resultado: " + resp;
+}
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "nada..."
+    }
 }
